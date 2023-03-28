a849dc4de1c29b0ab11a3886b648c0b78128b3fb
B3rnd1 <berndi.hahn@hotmail.com>
2023-03-28 11:21:26 +0200

    modified:   LoginStudent/JSLoginS.js 	modified:   LoginStudent/indexLoginS.html

--- LoginStudent/JSLoginS.js
+++ LoginStudent/JSLoginS.js
@@ -1,18 +1,42 @@
-let signupBtn = document.getElementById("signupBtn");
-let signinBtn = document.getElementById("signinBtn");
+let signupBtnStudent = document.getElementById("signupBtnStudent");
+let signupBtnCompany = document.getElementById("signupBtnCompany");
+let signinBtnStudent = document.getElementById("signinBtnStudent");
+let signinBtnCompany = document.getElementById("signinBtnCompany");
 let nameField = document.getElementById("nameField");
 let title = document.getElementById("title");
- 
-signinBtn.onclick = function(){
+
+signinBtnStudent.onclick = function(){
     nameField.style.maxHeight= "0";
-    title.innerHTML = "Sign In";
-    signupBtn.classList.add("disable");
-    signinBtn.classList.remove("disable");
+    title.innerHTML = "Sign In as a Student";
+    signupBtnStudent.classList.add("disable");
+    signinBtnStudent.classList.remove("disable");
+    signupBtnCompany.classList.add("disable");
+    signinBtnCompany.classList.add("disable");
 };
 
-signupBtn.onclick = function(){
+signinBtnCompany.onclick = function(){
+    nameField.style.maxHeight= "0";
+    title.innerHTML = "Sign In as a Company";
+    signupBtnCompany.classList.add("disable");
+    signinBtnCompany.classList.remove("disable");
+    signupBtnStudent.classList.add("disable");
+    signinBtnStudent.classList.add("disable");
+};
+
+signupBtnStudent.onclick = function(){
     nameField.style.maxHeight= "60px";
-    title.innerHTML = "Sign Up";
-    signupBtn.classList.remove("disable");
-    signinBtn.classList.add("disable");
+    title.innerHTML = "Sign Up as a Student";
+    signupBtnStudent.classList.remove("disable");
+    signinBtnStudent.classList.add("disable");
+    signupBtnCompany.classList.add("disable");
+    signinBtnCompany.classList.add("disable");
+};
+
+signupBtnCompany.onclick = function(){
+    nameField.style.maxHeight= "60px";
+    title.innerHTML = "Sign Up as a Company";
+    signupBtnCompany.classList.remove("disable");
+    signinBtnCompany.classList.add("disable");
+    signupBtnStudent.classList.add("disable");
+    signinBtnStudent.classList.add("disable");
 };
